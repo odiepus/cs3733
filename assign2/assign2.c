a54525b3d53aa1eef52d3a7e6055efd2353ca9ee
Hector Herrera <odiepus@gmail.com>
2017-06-26 21:37:11 -0500

    completed RR then oworked on some bugs. THinki i ironed em out

--- assign2/assign2.c
+++ assign2/assign2.c
@@ -1,23 +1,20 @@
 #include <stdio.h>
 #include "pslibrary.h"
+#include<errno.h>
+#include<stdlib.h>
+#include<string.h>
 
 int main(int argc, char *argv[])
 {
     int arraySize = 0;
-
 
     if(!(argc == 8)){
         printf("Usage: %s <param1> <param2> ... <param7>\nThe seven parameters must be integers\n", argv[0]);
         return -1;
     }
 
-    printf("Assignment 1 program written by Hector Herrera\n");
-
-    printf("inputs: ");
-
     int i = 1;
     for(; i < argc; i++){
-        printf("%c ", *argv[i]);
         if(i > 1){
             arraySize += *argv[i] - 48;
         }
@@ -27,19 +24,46 @@ int main(int argc, char *argv[])
         value[i-2] = *argv[i] - 48;
     }
 
-    char str1[arraySize];
-    char str2[arraySize];
-
-    //part0(str1, str2);
-//    proto(str1, str2, 0, value[0], value[1], value[2], value[3], value[4], value[5]);
-//    display("FCFS\n", str1, str2);
-
-//    sjf(str1, str2, value[0], value[1], value[2], value[3], value[4], value[5]);
-//    display("SJF\n", str1, str2);
-
-    psjf(str1, str2, value[0], value[1], value[2], value[3], value[4], value[5]);
-    display("PSJF\n", str1, str2);
+    char path[100] = "REPORT.txt";
+    FILE *fp;
+    fp = fopen(path, "a+");
+    if(!fp){
+        printf( "Error code opening file: %d\n", errno );
+        printf( "Error opening file: %s\n", strerror( errno ) );
+        exit (1);
+    }
 
 
+    printf("Assignment 1 program written by Hector Herrera\n");
+    fprintf(fp, "\nAssignment 1 program written by Hector Herrera\n");
+
+    printf("Inputs: %d %d %d %d %d %d %d\n", *argv[1]-48, value[0], value[1], value[2], value[3], value[4], value[5]);
+    fprintf(fp, "Values: %d %d %d %d %d %d %d\n", *argv[1]-48, value[0], value[1], value[2], value[3], value[4], value[5]);
+
+    fclose(fp);
+
+    char str1Proto[arraySize];
+    char str2Proto[arraySize];
+
+    char str1SFJ[arraySize];
+    char str2SJF[arraySize];
+
+    char str1PSJF[arraySize];
+    char str2PSJF[arraySize];
+
+    char str1RR[arraySize];
+    char str2RR[arraySize];
+
+    proto(str1Proto, str2Proto, value[0], value[1], value[2], value[3], value[4], value[5]);
+    display("FCFS\n", str1Proto, str2Proto);
+
+    sjf(str1SFJ, str2SJF, value[0], value[1], value[2], value[3], value[4], value[5]);
+    display("SJF\n", str1SFJ, str2SJF);
+
+    psjf(str1PSJF, str2PSJF, value[0], value[1], value[2], value[3], value[4], value[5]);
+    display("PSJF\n", str1PSJF, str2PSJF);
+
+    rr(str1RR, str2RR, *argv[1]-48, value[0], value[1], value[2], value[3], value[4], value[5]);
+    display("RR\n", str1RR, str2RR);
     return 0;
 }
--- assign2/pslibrary.c
+++ assign2/pslibrary.c
@@ -1,4 +1,7 @@
 #include<stdio.h>
+#include<string.h>
+#include<errno.h>
+#include<stdlib.h>
 
 #define READY  0
 #define RUNNING  1
@@ -19,7 +22,7 @@ static char stateChars[] = {'r','R','w',0};
 */
 /* assume s1 and s2 point to buffers with enough space to hold the result */
 /* assume that the int parameters are strictly greater than 0 */
-void proto(char *s1, char *s2, int quantum, int x1, int y1, int z1,
+void proto(char *s1, char *s2, int x1, int y1, int z1,
            int x2, int y2, int z2) {
     int i;                                   /* next string position (time) */
     int state1 = READY;                            /* start with both ready */
@@ -259,6 +262,137 @@ void psjf(char *s1, char *s2, int x1, int y1, int z1,
         }
     }                                               /* end of main for loop */
 }
+
+void rr(char *s1, char *s2, int q, int x1, int y1, int z1,
+        int x2, int y2, int z2){
+    int i;                                   /* next string position (time) */
+    int state1 = READY;                            /* start with both ready */
+    int state2 = READY;
+    int cpuLeft1 = x1;                       /* P1 next CPU burst remaining */
+    int cpuLeft2 = x2;                       /* P2 next CPU burst remaining */
+    int ioLeft1 = y1;        /* P1 next IO burst remaining, 0 if no more IO */
+    int ioLeft2 = y2;        /* P2 next IO burst remaining, 0 if no more IO */
+    int swapped = 0;
+    int k = 0;
+
+    for (i=0; (state1 != DONE) || (state2 != DONE); i++,k++) {
+        if ( k == q && swapped == 0){
+                swapped = 1;
+        }
+        else if(k == q && swapped == 1){
+                swapped = 0;
+        }
+
+        if(state1 == DONE){
+            swapped = 1;
+            if(k == q){
+                k = 0;
+            }
+        }
+        if(state2 == DONE){
+            swapped = 0;
+            if(k == q){
+                k = 0;
+            }
+        }
+
+        /* running process completes its CPU burst */
+        if ((state1 == RUNNING) && (cpuLeft1== 0)) {
+            if (ioLeft1 == 0) {
+                state1 = DONE;
+                s1[i] = stateChars[state1];            /* terminate the string */
+            }
+            else
+                state1 = WAITING;
+        }
+        else if ((state2 == RUNNING) && (cpuLeft2 == 0) ) {
+            if (ioLeft2 ==0){
+                state2 = DONE;
+                s2[i] = stateChars[state2];
+            }
+            else
+                state2 = WAITING;
+        }
+        /* handle IO complete */
+        if ((state1 == WAITING) && (ioLeft1 == 0)) {
+            state1 = READY;
+            cpuLeft1 = z1;
+            if(state2 == DONE || state2 == WAITING)state1 = RUNNING;
+        }
+        if ((state2 == WAITING) && (ioLeft2 == 0)) {
+            state2 = READY;
+            cpuLeft2 = z2;
+            if(state1 == DONE || state1 == WAITING){
+                state2 = RUNNING;
+            }
+        }
+        /* if both ready, depends on algorithm */
+        if ( (state1 == READY) && (state2 == READY)){
+            if(k <= q){
+                if(swapped){
+                    state2 = RUNNING;
+                    if (k == q){
+                        k = 0;
+                    }
+                }
+                else{
+                    state1 = RUNNING;
+                    if (k == q){
+                        k = 0;
+                    }
+                }
+            }
+            else if(k == 0){
+                state1 = RUNNING;
+            }
+        }
+        /* handle one ready and CPU available */
+        else if ( (state1 == READY ) && state2 != RUNNING && k == q && swapped == 0) {
+            state1 = RUNNING;
+            k = 0;
+        }
+        else if ( (state2 == READY) && (state1 != RUNNING && k == q && swapped == 1) ) {
+                state2 = RUNNING;
+                k = 0;
+        }
+        else if(state1 == RUNNING && state2 == READY && k == q){
+            state1 = READY;
+            state2 = RUNNING;
+            k = 0;
+        }
+        else if(state1 == READY && state2 == RUNNING && k == q){
+            state1 = RUNNING;
+            state2 = READY;
+            k = 0;
+        }
+        else if(state1 == READY && state2 == DONE){
+            state1 = RUNNING;
+        }
+        else if(state2 == READY && state1 == DONE){
+            state2 = RUNNING;
+        }
+
+        if(k == 3 && state1 == WAITING && state2 == WAITING){
+            k = 0;
+        }
+        /* insert chars in string, but avoid putting in extra string terminators */
+        if (state1 != DONE)
+            s1[i] = stateChars[state1];
+        if (state2 != DONE)
+            s2[i] = stateChars[state2];
+        /* decrement counts */
+        if (state1 == RUNNING)
+            cpuLeft1--;
+        if (state1 == WAITING)
+            ioLeft1--;
+        if (state2 == RUNNING)
+            cpuLeft2--;
+        if (state2 == WAITING)
+            ioLeft2--;
+    }
+}
+
+
 void display(char *heading, char *s1, char *s2){
 
     int length2 = 0;
@@ -304,20 +438,40 @@ void display(char *heading, char *s1, char *s2){
         larger = length1;
     }
 
+    char path[100] = "REPORT.txt";
+    FILE *fp;
+    fp = fopen(path, "a+");
+    if(!fp){
+        printf( "Error code opening file: %d\n", errno );
+        printf( "Error opening file: %s\n", strerror( errno ) );
+        exit (1);
+    }
+
     double rAvg = (r1 + r2)/2;
     double cpuUsage = bigR/larger;
-    s1[length1] = NULL;
-    s2[length2] = NULL;
+    s1[length1] = 0;
+    s2[length2] = 0;
 
-//    printf("\n%d\n%d\n", length1, length2);
-//    printf("\nXXXX %.5f, %.5f, %.5f\n", larger, bigR, cpuUsage);
-//    printf("\n\nk = %d\n\n", k);
+    //    printf("\n%d\n%d\n", length1, length2);
+    //    printf("\nXXXX %.5f, %.5f, %.5f\n", larger, bigR, cpuUsage);
+    //    printf("\n\nk = %d\n\n", k);
+
+
     printf("\n\n");
+    fprintf(fp, "\n\n");
     printf("%s", heading);
+    fprintf(fp, "%s", heading);
+
     printf("%s\n", s1);
+    fprintf(fp, "%s\n", s1);
     printf("%s\n", s2);
+    fprintf(fp, "%s\n", s2);
     printf("%.0f %.0f %.1f %.5f", r1, r2, rAvg, cpuUsage);
+    fprintf(fp, "%.0f %.0f %.1f %.5f", r1, r2, rAvg, cpuUsage);
     printf("\n");
+    fprintf(fp, "\n");
+
+    fclose(fp);
 }
 
 void fcfsa(char *s1, char *s2, int x1, int y1, int z1, int x2, int y2, int z2){
